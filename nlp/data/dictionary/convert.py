6364756aa26d8fc38c23599fec7519b85b76a5a8
chenxk <chenxk@guahao.com>
2020-12-09 19:43:47 +0800

    add: 增加医学词典

---
+++ nlp/data/dictionary/convert.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+word_dict = {
+    'body_part': 'nhb',
+    'crowd': 'nhc',
+    'disease': 'nhd',
+    'examination': 'nhe',
+    'hospital': 'nth',
+    'inspection': 'nhi',
+    'medicine': 'nhm',
+    'physical': 'nhp',
+    'symptom': 'nhs',
+    'treatment': 'nht',
+    'unit': 'wh'
+}
+
+
+def convert():
+    for key in word_dict.keys():
+        df = pd.read_csv(key + '.dic.default', sep='\t')
+        result = []
+        for index, row in df.iterrows():
+            result.append([row[0], word_dict[key], 1])
+        pd.DataFrame(result).to_csv(key + '.txt', index=None, header=None, encoding='utf-8', sep='\t')
+
+
+if __name__ == '__main__':
+    convert()
